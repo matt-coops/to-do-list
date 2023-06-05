e2036bbfad226a60f3ec3df86a02df968499061a
matt-coops <matt.cooper.85@gmail.com>
2023-06-05 17:29:24 +1000

    More work

--- script.js
+++ script.js
@@ -2,60 +2,81 @@
 
 const container = document.querySelector("#container");
 const createProject = document.querySelector("#create--project");
-const inputProject = document.querySelector("#new--project--input");
+const inputProject = document.querySelector(".new--project--input");
+const inputDesc = document.querySelector(".new--project--desc--input");
 
 const Controller = function () {};
 
-const Desktop = (function () {
+const Projects = (function () {
   const listProjects = [
-    { projectName: "Project 1", todos: ["Test", "Test2", "Test3"] },
-    { projectName: "Project 2", todos: ["Test", "Test2", "Test3"] },
-    { projectName: "Project 3", todos: ["Test", "Test2", "Test3"] },
-    { projectName: "Project 4", todos: ["Test", "Test2", "Test3"] },
-    { projectName: "Project 5", todos: ["Test", "Test2", "Test3"] },
+    {
+      projectName: "Project 0",
+      projectDesc: "this is a test",
+      todos: ["Test", "Test2", "Test3"],
+    },
+    {
+      projectName: "Project 1",
+      projectDesc: "this is a test",
+      todos: ["Test", "Test2", "Test3"],
+    },
+    {
+      projectName: "Project 2",
+      projectDesc: "this is a test",
+      todos: ["Test", "Test2", "Test3"],
+    },
   ];
-  const newProject = (projectName) => {
-    listProjects.push({ projectName: `${projectName}` });
-    render();
+
+  const newProject = (projectName, projectDesc, dueDate, priority) => {
+    listProjects.push({
+      projectName: `${projectName}`,
+      projectDesc: `${projectDesc}`,
+      dueDate: `${dueDate}`,
+      priority: `${priority}`,
+    });
+    View.renderProjects();
   };
   const getListProjects = () => listProjects;
-  return { newProject, getListProjects };
-})();
 
-const Projects = (function () {
-  const listTodo = [];
-
-  const setListTodo = (todo) => listTodo.push(todo);
-  const getListTodo = () => listTodo;
-  const newTodo = (title, description, dueDate, priority) => {
+  const newTodo = () => {
     return { title, description, dueDate, priority };
   };
 
-  return { newTodo, setListTodo, getListTodo };
+  return { newTodo, getListProjects, newProject };
 })();
 
-const render = function () {
-  container.innerHTML = "";
-  for (const x of Desktop.getListProjects()) {
-    const html = `
-    <div class="project">
-        <button class="edit--project">✏️</button>
-        <button class="delete--project">❌</button>
-        <p>${x.projectName}</p>
-      </div>
-    `;
-    container.insertAdjacentHTML("afterbegin", html);
-  }
-};
+const View = (function () {
+  const renderProjects = () => {
+    container.innerHTML = "";
+    for (const [i, v] of Projects.getListProjects().entries()) {
+      const html = `
+      <div class="project" data-project="${i}">
+      <button class="project--btn delete--project">❌</button>
+          <button class="project--btn edit--project">✏️</button>
+          <p>${v.projectName}<br>
+          <span class="description">${
+            v.projectDesc ? v.projectDesc : ""
+          }</span></p>
+        </div>
+      `;
+      container.insertAdjacentHTML("afterbegin", html);
+    }
+  };
+  return { renderProjects };
+})();
 
 createProject.addEventListener("click", function (e) {
   if (!inputProject.value) return;
-  Desktop.newProject(inputProject.value);
-  console.log(Desktop.getListProjects());
+  console.log(inputDesc.value);
+  Projects.newProject(inputProject.value, inputDesc.value);
+  inputDesc.value = "";
+  inputProject.value = "";
 });
 
-render();
+View.renderProjects();
 
-Projects.setListTodo(Projects.newTodo("Titler", "Desc", "Tomorrow", "Low"));
-Projects.setListTodo(Projects.newTodo("Titler", "Desc", "Tomorrow", "Low"));
-Projects.setListTodo(Projects.newTodo("Titler", "Desc", "Tomorrow", "Low"));
+container.addEventListener("click", function (e) {
+  const project = e.target.closest(".project");
+  if (e.target.classList.contains("delete--project")) {
+    console.log("true");
+  }
+});
